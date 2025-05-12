df00295f1ee882e04bb0abdfb97508f933a44a90
quanndb <quan2082003@gmail.com>
2025-05-13 00:15:32 +0700

    UI/ fix loading indicator

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,10 +1,4 @@
-// components/common/LoadingWrapper.client.tsx
-"use client";
-import { useLoading } from "@/store/LoadingStore";
-
-// Ensures that this component is only rendered on the client-side
-
-const LoadingIndicator = () => {
+export default function Loading() {
   return (
     <div className="fixed top-0 left-0 w-full h-full bg-white opacity-50 flex items-center justify-center z-50">
       <div className="w-16 h-16 border-8 border-solid rounded-full relative">
@@ -12,10 +6,4 @@ const LoadingIndicator = () => {
       </div>
     </div>
   );
-};
-
-export default function LoadingWrapper() {
-  const { isLoading } = useLoading();
-
-  return isLoading ? <LoadingIndicator /> : null;
 }
